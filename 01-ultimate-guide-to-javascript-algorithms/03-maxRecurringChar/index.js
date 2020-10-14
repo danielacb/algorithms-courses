a129b08bff01d805e589b990221aad17ec1acd5b
Daniela C. Barbosa <contato@danielacb.com>
2020-10-14 00:45:55 -0300

    challenge 03: code script

--- 01-ultimate-guide-to-javascript-algorithms/03-maxRecurringChar/index.js
+++ 01-ultimate-guide-to-javascript-algorithms/03-maxRecurringChar/index.js
@@ -4,9 +4,82 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 */
 
 
-
+// Using for of and for in loops
 function maxRecurringChar(text) {
-    // Code goes here
+    let charMap = {};
+    let maxValue = 0;
+    let maxChar = '';
+
+    for (let char of text) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > maxValue) {
+            maxValue = charMap[char];
+            maxChar = char;
+        }
+    }
+    return char;
+}
+
+// using reduce and for in loop
+function maxRecurringChar(text) {
+    let maxValue = 0;
+    let maxChar = '';
+
+    const charMap = text.split('').reduce((object,char) => {
+        object[chat] ? object[char]++ : object[char] = 1;
+        return object;
+    }, {});
+
+    for (let char in charMap) {
+        if (charMap[char] > maxValue) {
+            maxValue = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
+
+
+// Using For of and Arrays from Character Map
+function maxRecurringChar(text) {
+    let charMap = {};
+
+    for (let char of text) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    const charArray = Object.keys(charMap);
+    const valuesArray = Object.values(charMap);
+    const maxCharValue = Math.max(...valuesArray);
+
+    return charArray[valuesArray.indexOf(maxCharValue)];
+}
+
+
+// Using reduce and Arrays from Character Map 
+function maxRecurringChar(text) {
+    const charMap = text.split('').reduce((object,char) => {
+        object[char] ? object[char]++ : object[char] = 1;
+        return object;
+    }, {});
+
+    const charArray = Object.keys(charMap);
+    const valuesArray = Object.values(charMap);
+    const maxCharValue = Math.max(...valuesArray);
+
+    return charArray[valuesArray.indexOf(maxCharValue)];
 }
 
 
