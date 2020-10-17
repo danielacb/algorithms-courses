0dbb33154bc3cbf4e715b95a9cc0b5b2b178aa72
Daniela C. Barbosa <contato@danielacb.com>
2020-10-17 20:01:08 -0300

    challenge 05: code script

--- 01-ultimate-guide-to-javascript-algorithms/05-palindromeChecker/index.js
+++ 01-ultimate-guide-to-javascript-algorithms/05-palindromeChecker/index.js
@@ -5,11 +5,30 @@ e.g palindromeChecker('racecar') // will return true
 
 
 
-
+// Chaining Built-in Methods
 function palindromeChecker(text) {
-    v// Code goes here
+    const reversedText = text.toLowerCase().split('').reverse().join('');
+    return reversedText === text;
 }
 
+// Using spread operator and reduce()
+function palindromeChecker(text) {
+    text = text.toLowerCase();
+    return [...text].reduce((a,b) => b + a) === text;
+}
 
+// Using every()
+function palindromeChecker(text) {
+    let charArray = text.toLowerCase().split('');
+    return charArray.every((letter, index) => letter === charArray[charArray.length - index - 1]);
+}
+
+// Using for loop
+function palindromeChecker(text) {
+    for(let i = 0; i < text.length / 2; i++) {
+        if(text[i] !== text[text.length - i - 1]) return false;
+    }
+    return true;
+}
 
 module.exports = palindromeChecker;
