aa1d65254279c0f8865559817d52dbc6538c56bd
Daniela C. Barbosa <contato@danielacb.com>
2020-10-18 23:20:34 -0300

    challenge 06: code script

--- 01-ultimate-guide-to-javascript-algorithms/06-hammingDistance/index.js
+++ 01-ultimate-guide-to-javascript-algorithms/06-hammingDistance/index.js
@@ -5,11 +5,39 @@ E.g hammingDistance('rover', 'river') // should return 1
 
 
 
+// Using for loops
+function hammingDistance(strA, strB) {
+    strA = strA.toLowerCase();
+    strB = strB.toLowerCase();
+    let result = 0;
 
-function hammingDistance(stringA, stringB) {
-    // Code goes here
+    if (strA.length === strB.length) {
+        for (i = 0; i < strA.length; i++) {
+            if (strA[i] !== strB[i]) {
+                result++;
+            }
+        }
+        return result;
+    } else {throw new Error('Strings do not have equal length!')}
 }
 
+// Using while
+function hammingDistance(strA,strB) {
+    strA = strA.toLowerCase();
+    strB = strB.toLowerCase();
+    let i = 0;
+    let result = 0;
+
+    if (strA.length === strB.length) {
+        while (i < strA.length) {
+            if (strA[i] !== strB[i]) {
+                result++;
+            }
+            i++
+        }
+        return result;
+    } else {throw new Error('Strings do not have equal length!')}
+}
 
 
 module.exports = hammingDistance
