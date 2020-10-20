8d8cdaff104fa82659cf99e72cc741f1e3555de9
Daniela C. Barbosa <contato@danielacb.com>
2020-10-19 23:14:56 -0300

    challenge 07: code script

--- 01-ultimate-guide-to-javascript-algorithms/07-longestWord/index.js
+++ 01-ultimate-guide-to-javascript-algorithms/07-longestWord/index.js
@@ -4,10 +4,37 @@ E.g longestWord('Top Shelf Web Development Training on Scotch') //should return 
 */
 
 
-
+// Using for loop
 function longestWord(text) {
-    // Code goes here
+    text = text.split(' ');
+    let longest = '';
+
+    for (let word of text) {
+        if (word.length > longest.length) {
+            longest = word;
+        }
+    }
+
+    return longest;
 }
 
+
+// Using reduce()
+function longestWord(text) {
+    return text.split(' ').reduce((currentWord, maxWord) => {
+        if (currentWord.length > maxWord.length) {
+            return currentWord;
+        } else {
+            return maxWord;
+        }
+    });
+}
+
+
+// Using sort()
+function longestWord(text) {
+    text = text.split(' ').sort((a,b) => b.length - a.length);
+    return text[0];
+}
 
 module.exports = longestWord
