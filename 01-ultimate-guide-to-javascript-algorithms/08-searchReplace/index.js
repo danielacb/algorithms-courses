845b984bb508724b24a58705fe8dd9416a96da74
Daniela C. Barbosa <contato@danielacb.com>
2020-10-20 21:27:49 -0300

    challenge 08: starter code

---
+++ 01-ultimate-guide-to-javascript-algorithms/08-searchReplace/index.js
@@ -0,0 +1,8 @@
+/* CHALLENGE
+searchReplace("He is Sleeping on the couch", "Sleeping", "sitting") 
+should return "He is Sitting on the couch"
+*/
+
+function searchReplace(str,word,newWord) {
+  // Code goes here
+}
