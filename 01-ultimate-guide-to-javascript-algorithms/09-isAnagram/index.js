828c95acf14594bcaf25d2d8a0a1bfafc89517ce
Daniela C. Barbosa <contato@danielacb.com>
2020-10-22 01:25:01 -0300

    challenge 09: code script

--- 01-ultimate-guide-to-javascript-algorithms/09-isAnagram/index.js
+++ 01-ultimate-guide-to-javascript-algorithms/09-isAnagram/index.js
@@ -6,9 +6,68 @@ don't. E.g
 */
 
 
-
+// Using Direct comparison
 function isAnagram(stringA, stringB) {
-    // Code goes here
+    function sanitizeString(str) {
+        return str.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('');
+    }
+    return sanitizeString(stringA) == sanitizeString(stringB);
+}
+
+
+// Using for loop to creat charMap
+function isAnagram(stringA, stringB) {
+    function createCharMap(str) {
+        const charMap = {};
+
+        for (let char of str) {
+            if (charMap.hasOwnProperty(char)) {
+                charMap[char]++;
+            } else {
+                charMap[char] = 1;
+            }
+        }
+
+        return charMap;
+    }
+
+    if (stringA.length === stringB.length) {
+        const stringAMap = createCharMap(stringA);
+        const stringBMap = createCharMap(stringB);
+
+        for (let char in stringAMap) {
+            if (stringAMap[char] !== stringBMap[char]) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Using reduce() to create charMap
+function isAnagram(stringA, stringB) {
+    function createCharMap(str) {
+        return str.split('').reduce((object,item) => {
+            object[item] ? object[item]++ : object[item] = 1;
+            return object
+        }, {})
+    }
+
+    if (stringA.length === stringB.length) {
+        const stringAMap = createCharMap(stringA);
+        const stringBMap = createCharMap(stringB);
+
+        for (let char in stringAMap) {
+            if (stringAMap[char] !== stringBMap[char]) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
