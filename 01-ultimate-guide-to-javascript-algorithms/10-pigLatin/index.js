b055ecd872f349f462d517c3da1d1bedc60c5dcc
Daniela C. Barbosa <contato@danielacb.com>
2020-10-24 13:21:13 -0300

    challenge 10: code script

--- 01-ultimate-guide-to-javascript-algorithms/10-pigLatin/index.js
+++ 01-ultimate-guide-to-javascript-algorithms/10-pigLatin/index.js
@@ -9,8 +9,29 @@ Translate the provided string to pig latin by following the rules below:
   - "explain" = "explainway”
 */
 
+// Imperative Approach
 function pigLatin(str) {
-  // Code goes here
+  str = str.toLowerCase();
+  const vowels = ['a','e','i','o','u'];
+  let vowelIndex = 0;
+
+  if (vowels.includes(str[0])) {
+    return str +  'way';
+  } else {
+    for (let char of str) {
+      if (vowels.includes(char)) {
+        vowelIndex = str.indexOf(char);
+        break;
+      }
+    }
+    return str.slice(vowelIndex) + str.slice(0,vowelIndex) + 'ay'
+  }
+}
+
+
+// Declarative Approach
+function pigLatin(str) {
+  return str.replace(/^([aeiouy])(._)/, '$1$2way').replace(/^(_[_^aeiouy]+)(._)/, '$2$1ay');
 }
 
 module.exports = pigLatin;
