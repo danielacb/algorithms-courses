86f7a7c02ee53a1bdd07610b316ad3fcc1775892
Daniela C. Barbosa <contato@danielacb.com>
2020-10-28 00:13:31 -0300

    challenge 12: code script

--- 01-ultimate-guide-to-javascript-algorithms/12-mergeArrays/index.js
+++ 01-ultimate-guide-to-javascript-algorithms/12-mergeArrays/index.js
@@ -4,8 +4,19 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// Using Sets and Array.from()
 function mergeArrays(...arrays) {
+    let jointArray = []
+    
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    });
 
+    return Array.from(new Set([...jointArray]));
+}
+
+// Using Sets and spread operator
+function mergeArrays(...arrays) {
     let jointArray = []
     
     arrays.forEach(array => {
@@ -13,8 +24,33 @@ function mergeArrays(...arrays) {
     });
 
     return [...new Set([...jointArray])]
+}
 
-    
+// Using filter()
+function mergeArrays(...arrays) {
+    let jointArray = [];
+    arrays.forEach(array => {
+        jointArray = [...jointArray,...array];
+    });
+
+    return jointArray.filter((item,index) => jointArray.indexOf(item) === index);
+}
+
+// Using reduce()
+function mergeArrays(...arrays) {
+    let jointArray = [];
+
+    arrays.forEach(array => {
+        jointArray = [...jointArray,...array];
+    });
+
+    return jointArray.reduce((newArray,item) => {
+        if (newArray.includes(item)) {
+            return newArray;
+        } else {
+            return [...newArray,item];
+        }
+    }, [])
 }
 
 
