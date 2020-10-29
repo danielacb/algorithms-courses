86859ba367e153677036515309f0b0149aaa35e1
Daniela C. Barbosa <contato@danielacb.com>
2020-10-29 00:18:37 -0300

    challenge 13: code script

--- 01-ultimate-guide-to-javascript-algorithms/13-falsyBouncer/index.js
+++ 01-ultimate-guide-to-javascript-algorithms/13-falsyBouncer/index.js
@@ -5,9 +5,35 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-
+// Using forEach()
 function falsyBouncer(array) {
-    // Code goes here
+    const result = [];
+
+    array.forEach(item => {
+        if (item) {
+            result.push(item);
+        }
+    });
+
+    return result;
+}
+
+// Using for...of
+function falsyBouncer(array) {
+    const result = [];
+
+    for (value of array) {
+        if (value) {
+            result.push(value);
+        }
+    }
+
+    return result;
+}
+
+// Using filter()
+function falsyBouncer(array) {
+    return array.filter(value => Boolean(value));
 }
 
 
