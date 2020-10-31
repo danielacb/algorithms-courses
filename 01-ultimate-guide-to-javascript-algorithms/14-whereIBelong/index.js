b2b3496eb174345797da81adcda782458ca531ec
Daniela C. Barbosa <contato@danielacb.com>
2020-10-31 00:14:34 -0300

    challenge 14: code script

--- 01-ultimate-guide-to-javascript-algorithms/14-whereIBelong/index.js
+++ 01-ultimate-guide-to-javascript-algorithms/14-whereIBelong/index.js
@@ -7,12 +7,51 @@ whereIBelong([1,2,3,4], 1.5) // should return 1 because it is greater
 than 1(index 0), but less than 2(index 1).
 */
 
-
-
+// Using a For-loop to find the immediate larger value's position
 function whereIBelong(arr, num) {
-   // Code goes here
+   arr = arr.sort((a,b) => a - b);
+   for (let i = 0; i < arr.length; i++) {
+     if (arr[i] >= num) {
+      return i;
+     } 
+   }
+   return arr.length;
+ }
+
+
+// Using a For-loop to determine the number of smaller values
+function whereIBelong(arr, num) {
+   arr = arr.sort((a,b) => a - b);
+   let counter = 0;
+
+   for (let i = 0; i < arr.length; i++) {
+      if (num > arr[i]) {
+         counter++;
+      }
+   }
+
+   return counter;
 }
 
+// Using a While Loop
+function whereIBelong(arr, num) {
+   arr = arr.sort((a,b) => a - b);
+   let counter = 0;
+
+   while (num > arr[counter]) {
+      counter++;
+   }
+
+   return counter;
+}
+
+
+// Finding the index of the value directly
+function whereIBelong(arr, num) {
+   arr.push(num);
+   arr.sort((a,b) => a - b);
+   return arr.indexOf(num);
+}
 
 
 module.exports = whereIBelong
