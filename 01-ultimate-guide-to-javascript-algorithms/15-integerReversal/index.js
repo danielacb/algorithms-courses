573065477f5ac4a36d5f6c39bccd3dd4e18bd3ea
Daniela C. Barbosa <contato@danielacb.com>
2020-11-01 20:51:51 -0300

    challenge 15: starter code

---
+++ 01-ultimate-guide-to-javascript-algorithms/15-integerReversal/index.js
@@ -0,0 +1,9 @@
+/* CHALLENGE
+Given an integer, return an integer that is the reverse 
+ordering of that which was received. E.g
+    reverseInteger(-123) // should return -321
+*/
+
+function reverseString(text) {
+  
+}
